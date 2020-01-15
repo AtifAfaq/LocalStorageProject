933e6188ed7565b0a68a78cb501b298d8fe5e919
Muhammad Afaq <43587275+AtifAfaq@users.noreply.github.com>
2020-01-15 15:41:58 -0500

    check email

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -14,6 +14,7 @@ export class FormsComponent implements OnInit {
   password: string = "";
   myArray: any = [];
   array: any = [];
+  y: any = ""
   constructor(public fb: FormBuilder, public router: Router) { }
 
   ngOnInit() {
@@ -50,15 +51,24 @@ export class FormsComponent implements OnInit {
     if (!this.myArray) {
       this.myArray = [];
     }
-    this.myArray.push(User);
+    debugger;
+    this.myArray.map(x => {
+      x.email;
+      if (x.email == this.email) {
+        alert("Email Already in used");
+        return
+        this.myArray.push(User);
+        localStorage.setItem('users', JSON.stringify(this.myArray));
+        this.router.navigate(['/home']);
+        this.SignUp.reset();
+      }
+    });
 
-    localStorage.setItem('users', JSON.stringify(this.myArray));
 
     //  to view the array
     // var mydata = localStorage.getItem('users');
     // this.array = JSON.parse(mydata);
-    this.router.navigate(['/home']);
-    this.SignUp.reset();
+
 
   }
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
   myArray: any = [];
   Currentuser: any = {};
+  user: any = "";
+  index: any = "";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    var retreivedUsers = localStorage.getItem('users');
+    var retreivedUsers = localStorage.getItem("users");
 
     this.myArray = JSON.parse(retreivedUsers);
-
   }
   debugger;
 
   showModal(i) {
-    this.Currentuser = this.myArray[i]
-
+    this.Currentuser = this.myArray[i];
   }
 
-  deleteData(i) {
-    this.myArray.splice(i, 1);
-    localStorage.setItem('users', JSON.stringify(this.myArray));
-    console.log(this.myArray)
+  confirmDelete(i) {
+    debugger;
+    this.Currentuser = this.myArray[i];
+    this.index = i;
+
+    console.log(this.myArray);
+  }
+
+  deleteData(index) {
+    debugger;
+    this.myArray.splice(index, 1);
+    localStorage.setItem("users", JSON.stringify(this.myArray));
   }
 
   updateData() {
-    console.log(this.myArray)
-    localStorage.setItem('users', JSON.stringify(this.myArray));
-
-    console.log(this.myArray)
+    console.log(this.myArray);
+    localStorage.setItem("users", JSON.stringify(this.myArray));
+    console.log(this.myArray);
   }
 }
